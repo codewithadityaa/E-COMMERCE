237038f6280cad85175bb74400ed89bbeedb0d7d
codewithadityaa <114819528+codewithadityaa@users.noreply.github.com>
2024-03-24 03:20:52 +0530

    Update Navbar.jsx

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
-import nav_dropdown from '../Assets/dropdown_icon.png'
+import nav_dropdown from '../Assets/dpm.png'
 
 const Navbar = () => {
 
